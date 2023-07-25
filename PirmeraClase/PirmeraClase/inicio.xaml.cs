9bdd4dc91c72d41da4d1b747273f75df726d99ca
CARDONA <91144533+CarLuis07@users.noreply.github.com>
2023-07-25 11:43:15 -0600

    Agregar archivos de proyecto.

---
+++ PirmeraClase/PirmeraClase/inicio.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PirmeraClase
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class inicio : ContentPage
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new Pagina2());
+        }
+
+        private async void ButtonGoPagina2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pagina2());
+        }
+    }
+}
